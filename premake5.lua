84418a487f60272b3dfec7e7b8bd28c712388567
cybastephen <119753310+cybastephen@users.noreply.github.com>
2024-03-29 14:26:34 +0530

    updated submodule

--- premake5.lua
+++ premake5.lua
@@ -7,12 +7,35 @@ workspace "RenderingEngine"
     }
 
     outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    IncludeDir = {}
+    IncludeDir["GLFW"] = "vendor/GLFW/include"
+
+    include "vendor/GLFW"
+
 project "RenderingEngine"
     kind "ConsoleApp"
     language "C++"
 
     targetdir ("bin/" .. outputdir .. "%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "%{prj.name}")
+
+    files
+    {
+        "%{src/**.h}",
+        "%{src/**.cpp}"
+    }
+
+    includekdirs
+    {
+        "%src",
+        "%{includeDir.GLFW}"
+    }
+
+    links
+    {
+        "GLFW"
+    }
 
     filter "system:windows"
         cppdialect "C++17"
